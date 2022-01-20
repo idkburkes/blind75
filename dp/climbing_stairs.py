95e8d4b4d0266e8dd8a6a500fea5063656c094f4
Isaiah Burkes <idkburkes@gmail.com>
2022-01-20 14:19:15 -0600

    Added iterative and O(1) space solution to climbing stairs

--- dp/climbing_stairs.py
+++ dp/climbing_stairs.py
@@ -14,3 +14,31 @@ class Solution:
             dp[steps_remaining] = one_step + two_step
         
         return dp[steps_remaining]
+
+
+    # This climbing stairs problem is essentially the fibonacci sequence
+    # We can solve iteratively using bottom-up starting from the beginning
+    # of 1D dp array
+    def climbStairsIterative(self, n: int) -> int:
+        if n == 1: return 1
+        dp = [0 for i in range(n)]
+        dp[0], dp[1] = 1, 2
+        for i in range(2,n):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n-1]
+
+    # We can also use O(1) space remembering that to solve this sequence
+    # We only need to previous 2 numbers. We don't have to keep track of the entire
+    # DP array! This translates to more complicated dp problems where we only
+    # need to keep track of the previous row.
+    def climbStairsIterativeConstantSpace(self, n:int) -> int:
+        if n == 1:
+             return 1
+        elif n == 2:
+             return 2
+        a, b = 1, 2
+        for _ in range(2,n-1):
+            c = a + b
+            a = b
+            b = c
+        return a + b
